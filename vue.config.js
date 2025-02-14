bea9ad159248b41de67893bba77ffe16988e3e25
matias <matiasleites@live.com.ar>
2025-02-14 14:40:16 -0300

    feat: removing invalid code

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,6 @@ module.exports = defineConfig({
     port: process.env.PORT || 5000, 
     host: '0.0.0.0', 
     allowedHosts: 'all',
-    public: 'https://pc8jvg-5000.csb.app',
     https: true,
     hot: false,
   }
