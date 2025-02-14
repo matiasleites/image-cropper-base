b44899ee50475a9bd107b6b038836e3050d42010
matias <matiasleites@live.com.ar>
2025-02-14 12:46:28 -0300

    feat: adding port

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    port: process.env.PORT || 5000, // Usa el puerto asignado por CodeSandbox o 8080 por defecto
+    host: '0.0.0.0', // Acepta conexiones desde cualquier dirección
+  },
 })
